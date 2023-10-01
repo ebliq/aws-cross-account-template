418d5daffdfe339267fc94a039709afa070e0d71
philschmid <schmidphilipp1995@gmail.com>
2023-10-01 20:56:28 +0200

    stroke

--- app/src/components/icons/loading.tsx
+++ app/src/components/icons/loading.tsx
@@ -87,7 +87,7 @@ export function LoadingSpinner() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
             className="animate-[2.5s_ease-in-out_1s_infinite_normal_none_running_pulse] opacity-10"
           >
             <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z" />
