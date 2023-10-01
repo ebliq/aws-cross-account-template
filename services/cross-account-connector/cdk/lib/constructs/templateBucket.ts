2231203da6537a311931de98bb1124bed4f492c9
philschmid <schmidphilipp1995@gmail.com>
2023-10-01 16:28:14 +0200

    cleaned up cdk and created construct with template method

---
+++ services/cross-account-connector/cdk/lib/constructs/templateBucket.ts
@@ -0,0 +1,75 @@
+import { Construct } from 'constructs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as Handlebars from "handlebars";
+import * as fs from "fs";
+import * as path from "path";
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Fn } from 'aws-cdk-lib';
+
+export interface PublicBucketProps extends s3.BucketProps {
+  namePrefix: string;
+  randomSufix: string;
+}
+/*
+ * Public S3 bucket, which stores the Cloudformation template
+ * creates, S3 bucket, S3 bucket policy, S3 bucket policy statement
+ * implements template generate methond and uploads template to S3 bucket
+ */
+export class PublicTemplateBucket extends Construct {
+  public readonly publicTemplateBucket: s3.Bucket;
+
+  constructor(scope: Construct, id: string, props: PublicBucketProps) {
+    super(scope, id);
+    console.log(__dirname)
+
+    this.publicTemplateBucket = new s3.Bucket(this, id, {
+      bucketName: `${props.namePrefix}-public-template-bucket-${props.randomSufix}`,
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        ignorePublicAcls: false,
+        blockPublicPolicy: false,
+        restrictPublicBuckets: false,
+      }),
+    });
+
+    // Add bucket policy to allow public access to the bucket
+    this.publicTemplateBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        resources: [this.publicTemplateBucket.arnForObjects("*")],
+        actions: ["s3:GetObject"],
+        principals: [new iam.AnyPrincipal()],
+      })
+    );
+  }
+
+  /**
+   * Generates the CloudFormation template for this construct
+   */
+  public generateTemplate({ lambdaArn, awsAccountId }: { lambdaArn: string, awsAccountId: string }) {
+    // read template file 
+    const templateFile = fs.readFileSync(
+      path.join(__dirname, "../../templates/template.json"), // __dirname == lib/constructs
+      "utf8"
+    );
+    // compile template
+    const template = Handlebars.compile(templateFile);
+
+    // generate template
+    const generatedTemplate = template({ lambdaArn, awsAccountId });
+    return generatedTemplate;
+  }
+
+  /**
+   * Uploads the generated template to the S3 bucket
+   */
+  public uploadTemplate({ template }: { template: string }) {
+    // Upload template logic  
+    const object = new BucketDeployment(this, "UploadCloudformationTemplate", {
+      sources: [Source.data("template.json", template)],
+      destinationBucket: this.publicTemplateBucket,
+    })
+    return Fn.select(0, object.objectKeys);
+  }
+
+}
---
+++ services/cross-account-connector/cdk/lib/cross-account-connector-stack.ts
@@ -0,0 +1,85 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as pythonLambda from "@aws-cdk/aws-lambda-python-alpha";
+import { PublicTemplateBucket } from "./constructs/templateBucket";
+import * as path from "path";
+
+export interface CrossAccountConnectorProps extends cdk.StackProps {
+  namePrefix: string;
+  randomSufix: string;
+}
+
+
+export class CrossAccountConnectorStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: CrossAccountConnectorProps) {
+    super(scope, id, props);
+
+    console.log('accountId: ', this.account);
+    console.log('region: ', this.region);
+
+    // Create public template bucket which stores the Cloudformation template
+    const publicTemplateBucket = new PublicTemplateBucket(this, "publicTemplateBucket", {
+      namePrefix: props.namePrefix,
+      randomSufix: props.randomSufix,
+    });
+
+    // Create DynamoDB table for storing account information
+    const table = new dynamodb.Table(this, "table", {
+      partitionKey: { name: "PK", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "SK", type: dynamodb.AttributeType.STRING },
+      deletionProtection: false,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    // Creates Lambda function as custom resource Cloudformation resource, which is called during the stack creation
+    // it adds and removes the account information from the DynamoDB table
+    const functionName = `${props.namePrefix}-custom-resource-connector-${props.randomSufix}`
+    const connectorLambda = new pythonLambda.PythonFunction(
+      this,
+      "customResourceConnector",
+      {
+        functionName: functionName,
+        entry: path.join(__dirname, "../../src"), // __dirname == lib/
+        runtime: lambda.Runtime.PYTHON_3_11,
+        index: "handler.py",
+        handler: "handler",
+        environment: {
+          DYNAMODB_TABLE: table.tableName,
+        },
+        deadLetterQueueEnabled: true,
+      }
+    );
+    const connectorLambdaArn = `arn:aws:lambda:${this.region}:${this.account}:function:${functionName}`
+    // grant invoke permissions to the lambda function from any principal
+    // this is required for the custom resource to work
+    connectorLambda.grantInvoke(new iam.AnyPrincipal());
+    // grant read write permissions to the lambda function from the DynamoDB table
+    table.grantReadWriteData(connectorLambda);
+
+    // Generate the Cloudformation template
+    const template = publicTemplateBucket.generateTemplate({ lambdaArn: connectorLambdaArn, awsAccountId: this.account })
+    // Upload the Cloudformation template to the S3 bucket
+    const templateUri = publicTemplateBucket.uploadTemplate({ template });
+
+
+
+
+    // Outputs
+
+    // DynamoDB table name
+    new cdk.CfnOutput(this, "DynamoDBTableName", {
+      exportName: "DynamoDBTableName",
+      value: table.tableName,
+    });
+
+    // Cloudformation template URL
+    new cdk.CfnOutput(this, "CloudformationTemplateUrl", {
+      exportName: "CloudformationTemplateUrl",
+      value: templateUri,
+    });
+  }
+
+}
